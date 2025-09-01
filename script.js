fe23771612e7cbe853ec976041d183cd1df164aa
gwensttacy <rasikhahnailah@gmail.com>
2025-09-02 00:24:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `Happy failed mensive.\nI know we’re not together anymore, i know maybe you don’t love me like before. But one thing you should know, I still love and care about you. My feelings are real, they’re honest, and I’m still here, waiting.\n\n I know we both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nWas there a moment you wished we never let go?. Lio, I’m still here. I haven’t gone anywhere. I hope that one day, you’ll understand and feel the love I’ve been carrying for you all this time.`;
+const message = `Happy failed mensive.\nI know we’re not together anymore, i know maybe you don’t love me like before. But one thing you should know, I still love and care about you. My feelings are real, they’re honest, and I’m still here, waiting.\n\n I know we both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nIf i can ask you right know, was there a moment you wished we never let go?. Lio, I just want you to know if I’m still here. I haven’t gone anywhere. I hope that one day, you’ll understand and feel the love I’ve been carrying for you all this time.`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
@@ -21,4 +21,5 @@ function showLetter() {
     typeWriter();
   }, 600);
 }
+
 
